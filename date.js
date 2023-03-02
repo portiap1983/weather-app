840ed36045e4e44748f68f583e21fc8febe811f8
portiap1983 <119122664+portiap1983@users.noreply.github.com>
2023-03-01 19:53:00 -0800

    my weather project

---
+++ date.js
@@ -0,0 +1,76 @@
+import "./styles.css";
+
+let now = new Date();
+
+let h3 = document.querySelector("h3");
+
+let date = now.getDate();
+let year = now.getFullYear();
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = ["Sun", "Mon", "Tues", "Weds", "Thur", "Fri", "Sat"];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let month = months[now.getMonth()];
+
+h3.innerHTML = `${day} ${month} ${date}, ${year} ${hours}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+  searchCity(searchInput.value);
+}
+let form = document.querySelector("#search-form");
+
+form.addEventListener("submit", search);
+//////Week 5///////
+
+function searchCity(city) {
+  let apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showTemperature(response) {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${temperature}°C`;
+}
+
+///button location function///
+
+function showPosition(position) {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = "currentPosition";
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+}
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let button = document.querySelector("button");
+button.addEventListener("click", getCurrentPosition);
